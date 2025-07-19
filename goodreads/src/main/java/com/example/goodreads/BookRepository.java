4f39f867902e529da8c21da148fc6d009c413cef
bharath <bharathhalemane143@gmail.com>
2025-07-19 07:30:40 +0530

    second project

--- goodreads/src/main/java/com/example/goodreads/BookRepository.java
+++ goodreads/src/main/java/com/example/goodreads/BookRepository.java
@@ -3,6 +3,6 @@ package com.example.goodreads;
 import java.util.ArrayList;
 import com.example.goodreads.Book;
 
-public interface Bookrepository {
+public interface BookRepository {
         ArrayList<Book> book= new ArrayList<>();
 }
