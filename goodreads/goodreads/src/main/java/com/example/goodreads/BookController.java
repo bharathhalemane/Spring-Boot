6f5e29ab54a5cdf409df2f701a969e56388cae92
bharath <bharathhalemane143@gmail.com>
2025-07-19 06:53:15 +0530

    second project

---
+++ goodreads/goodreads/src/main/java/com/example/goodreads/BookController.java
@@ -0,0 +1,12 @@
+package com.example.goodreads;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class BookController{
+
+    @GetMapping("/book")
+
+
+}
